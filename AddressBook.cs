0bc36c116b863d88f0c8b6a1333717e0a745c011
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-03 13:16:22 -0500

    Adding more files

--- AddressBook.cs
+++ AddressBook.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 namespace address_book {
 
     public class AddressBook {
-        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List
     }
 }
