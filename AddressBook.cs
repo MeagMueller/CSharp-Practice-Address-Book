40e87a9928b6f9a22bbd996c6af67c03c76c4dfd
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-04 13:49:51 -0500

    Still working

--- AddressBook.cs
+++ AddressBook.cs
@@ -7,7 +7,11 @@ namespace address_book {
         public Dictionary<string, Contact> Contacts { get; set; } = new Dictionary<string, Contact>();
 
          public void AddContact(Contact Contact) {
-            
+            Contacts.Add(Contact.Email, Contact);
+        }
+
+        public Contact GetByEmail(string email) {
+            return Contacts[email];
         }
 
         
